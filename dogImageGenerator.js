49bc307e2df1a8d98d0da7383ae08fff5aa192e6
rmaharja@gmail.com <rmaharja@gmail.com>
2018-12-20 17:28:32 -0500

    data output correct match..Need to convert into json

--- dogImageGenerator.js
+++ dogImageGenerator.js
@@ -7,12 +7,19 @@ const expectedBreedCount = require('./expectedBreedCount.json');
 async function getBreedCount() {
     const response = await axios.get('https://dog.ceo/api/breeds/list/all');
     // COMPLETE MISSING CODE HERE:
-    console.log("response.data.message: ", response.data.message);
     
+    // console.log("response.data.message: ", response.data.message);
+    const dogsObject = response.data.message;
+    let x;
+
+    for (x in dogsObject) {
+    console.log(x, dogsObject[x].length);
+}
 
 /*
 ...Data returned is an object with key:value (breeds: [array of all subbreeds])
 -Loop through each array
+
 -if length of array in value is 0, return 0
 -if length of array is not 0, return length of array in each value pair
 */
