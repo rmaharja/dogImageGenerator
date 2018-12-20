9b4d90eeff65f2519af75d0fc650b554512eb2b0
rmaharja@gmail.com <rmaharja@gmail.com>
2018-12-20 17:16:26 -0500

    Pseudo code for breedCount done

--- dogImageGenerator.js
+++ dogImageGenerator.js
@@ -2,12 +2,23 @@ const axios = require('axios');
 const chai = require('chai');
 const expectedBreedCount = require('./expectedBreedCount.json');
 
+
 // Count all sub breeds for a breed. If no subbreeds, should read 0
 async function getBreedCount() {
     const response = await axios.get('https://dog.ceo/api/breeds/list/all');
     // COMPLETE MISSING CODE HERE:
-    console.log("Response: ", response);
-}
+    console.log("response.data.message: ", response.data.message);
+    
+
+/*
+...Data returned is an object with key:value (breeds: [array of all subbreeds])
+-Loop through each array
+-if length of array in value is 0, return 0
+-if length of array is not 0, return length of array in each value pair
+*/
+
+
+}//end of getBreedCount
 
 // Get a picture of sub-breed. Ex: 'basset'
 async function getRandomImageOfSubBreed(subBreed) {
